e23c74b8c1eabd4a7a44d6631e8a5caa639656ab
Ryan Balla <ryan@ryanballa.com>
2019-06-04 19:59:07 -0500

    Removing bio pic

--- src/components/bio.js
+++ src/components/bio.js
@@ -7,7 +7,6 @@
 
 import React from "react"
 import { StaticQuery, graphql } from "gatsby"
-import Image from "gatsby-image"
 
 import { rhythm } from "../utils/typography"
 
@@ -24,24 +23,11 @@ function Bio() {
               marginBottom: rhythm(2.5),
             }}
           >
-            <Image
-              fixed={data.avatar.childImageSharp.fixed}
-              alt={author}
-              style={{
-                marginRight: rhythm(1 / 2),
-                marginBottom: 0,
-                minWidth: 50,
-                borderRadius: `100%`,
-              }}
-              imgStyle={{
-                borderRadius: `50%`,
-              }}
-            />
             <p>
               Written by <strong>{author}</strong>, a Chicago-based model railroad group.
               {` `}
               <a href={`https://twitter.com/${social.twitter}`}>
-                You should follow him on Twitter
+                You should follow us on Twitter
               </a>
             </p>
           </div>
@@ -53,13 +39,13 @@ function Bio() {
 
 const bioQuery = graphql`
   query BioQuery {
-    avatar: file(absolutePath: { regex: "/profile-pic.jpg/" }) {
-      childImageSharp {
-        fixed(width: 50, height: 50) {
-          ...GatsbyImageSharpFixed
-        }
-      }
-    }
+    # avatar: file(absolutePath: { regex: "/profile-pic.jpg/" }) {
+    #   childImageSharp {
+    #     fixed(width: 50, height: 50) {
+    #       ...GatsbyImageSharpFixed
+    #     }
+    #   }
+    # }
     site {
       siteMetadata {
         author
--- src/pages/plan.js
+++ src/pages/plan.js
@@ -15,6 +15,9 @@ const StyledSection = styled('section')({
         justifyContent: 'center',
         paddingLeft: '10px',
     },
+    '& .inst': {
+        marginBottom: '10px',
+    },
     '& .selectOption': {
         '& li': {
             '&:after': {
@@ -42,6 +45,10 @@ const StyledSection = styled('section')({
                 background: '#2ebccc',
                 borderTop: '#134c52',
             },
+            '&.clicked': {
+                background: '#cbe3ef',
+                border: '1px solid #b0c5d0'
+            },
             background: '#FFFFFF',
             border: '1px solid #CCCCCC',
             boxShadow: '0px 1px 4px rgba(0, 0, 0, .1)',
@@ -60,7 +67,6 @@ const StyledSection = styled('section')({
             marginRight: 0,
         },
         '& li:hover': {
-            background: '#efefef',
             cursor: 'pointer',
         },
         fontSize: '75%',
@@ -74,8 +80,28 @@ const StyledSection = styled('section')({
 const PlanPage = () => {
 
     const [score, setScore] = useState({});
+    const [score2, setScore2] = useState({});
+    const [score3, setScore3] = useState({});
+    const [score4, setScore4] = useState({});
     const iconHeight = 33;
     const iconWidth = 33;
+
+    const handleAnswerSelection = (questionNumber, value) => {
+        switch (questionNumber) {
+            case 1:
+                setScore(value);
+            break;
+            case 2:
+                setScore2(value);
+            break;
+            case 3:
+                setScore3(value);
+            break;
+            case 4:
+                setScore3(value);
+            break;
+        }
+    }
 
     return (
         <Layout>
@@ -89,54 +115,63 @@ const PlanPage = () => {
                 <p>Answering a few questions will help us figure out what layout type is best for you.</p>
                 <img src={construction} alt="Layout under construction" width="200" />
                 <StyledSection>
-                    <h2><svg width="33" height="33" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="feather feather-clock">
+                    <h2><svg width="33" height="33" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-clock">
                         <circle cx="12" cy="12" r="10"></circle>
                         <polyline points="12 6 12 12 16 14"></polyline>
                     </svg>Time</h2>
                     
                     <p>Model railroading is approachable for anyone and their time allowances. From a little time to a lot of time you can get a lot of of the hobby.</p>
-                    <p><b>Select the amout of time you have available.</b></p>
+                    <p className="inst"><b>Select the amout of time you have available.</b></p>
                     <ul className="selectOption">
-                        <li className="first" onClick={() => setScore({1: 2})}><span>1 - 2 hours a month</span></li>
-                        <li className="second" onClick={() => setScore({1: 4})}><span>1 - 2 hours a week</span></li>
-                        <li className="third" onClick={() => setScore({1: 6})}><span>3 - 4 hours a week</span></li>
-                        <li className="fourth" onClick={() => setScore({1: 8})}><span>several hours a day</span></li>
+                        <li className={score === 2 ? 'clicked' : ''} onClick={() => handleAnswerSelection(1, 2)}><span>1 - 2 hours a month</span></li>
+                        <li className={score === 4 ? 'clicked' : ''} onClick={() => handleAnswerSelection(1, 4)}><span>1 - 2 hours a week</span></li>
+                        <li className={score === 6 ? 'clicked' : ''} onClick={() => handleAnswerSelection(1, 6)}><span>3 - 4 hours a week</span></li>
+                        <li className={score === 8 ? 'clicked' : ''} onClick={() => handleAnswerSelection(1, 8)}><span>several hours a day</span></li>
                     </ul>
                     <h2>
-                        <svg width={iconWidth} height={iconHeight} viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-pie-chart">
+                        <svg width={iconWidth} height={iconHeight} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-pie-chart">
                             <path d="M21.21 15.89A10 10 0 1 1 8 2.83"></path>
                             <path d="M22 12A10 10 0 0 0 12 2v10z"></path>
                         </svg>Collection Size</h2>
                     <p>From large to small anyone can find a fun entrace to the hobby.</p>
-                    <p><b>Select the size option that best matches your collection.</b></p>
+                    <p className="inst"><b>Select the size option that best matches your collection.</b></p>
                     <ul className="selectOption">
-                        <li><span>1 - 10 trains / railcars</span></li>
-                        <li><span>10 - 50 trains / railcars</span></li>
-                        <li><span>50 - 100 trains / railcars</span></li>
-                        <li><span>More than 100 trains / railcars</span></li>
+                        <li className={score2 === 2 ? 'clicked' : ''} onClick={() => handleAnswerSelection(2, 2)}><span>1 - 10 trains / railcars</span></li>
+                        <li className={score2 === 4 ? 'clicked' : ''} onClick={() => handleAnswerSelection(2, 4)}><span>10 - 50 trains / railcars</span></li>
+                        <li className={score2 === 6 ? 'clicked' : ''} onClick={() => handleAnswerSelection(2, 6)}><span>50 - 100 trains / railcars</span></li>
+                        <li className={score2 === 8 ? 'clicked' : ''} onClick={() => handleAnswerSelection(2, 8)}><span>More than 100 trains / railcars</span></li>
                     </ul>
                     <h2>
-                    <svg width={iconWidth} height={iconHeight}  viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="feather feather-home">
+                    <svg width={iconWidth} height={iconHeight}  viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-home">
                         <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path>
                         <polyline points="9 22 9 12 15 12 15 22"></polyline>
                     </svg>Space</h2>
                     <p>Apartment dwellers or folks with large basements can enjoy running trains on a layout.</p>
-                    <p><b>Select how much space you have to work with.</b></p>
+                    <p className="inst"><b>Select how much space you have to work with.</b></p>
                     <ul className="selectOption">
-                        <li><span>None</span></li>
-                        <li><span>Small amount (closet, table top)</span></li>
-                        <li><span>Medium amount (spare room, garage parking spot)</span></li>
-                        <li><span>Large amount (basement, shed)</span></li>
+                        <li className={score3 === 2 ? 'clicked' : ''} onClick={() => handleAnswerSelection(3, 2)}><span>None</span></li>
+                        <li className={score3 === 4 ? 'clicked' : ''} onClick={() => handleAnswerSelection(3, 4)}><span>Small amount (closet, table top)</span></li>
+                        <li className={score3 === 6 ? 'clicked' : ''} onClick={() => handleAnswerSelection(3, 6)}><span>Medium amount (spare room, garage parking spot)</span></li>
+                        <li className={score3 === 8 ? 'clicked' : ''} onClick={() => handleAnswerSelection(3, 8)}><span>Large amount (basement, shed)</span></li>
+                    </ul>
+                    <p>Everyone hopes to get something differet from the hobby. Some folks like seeing trains operate, others like the socialization, and you might enjoy the building or planning process. There is no wrong way to enjoy the hobby.</p>
+                    <p className="inst"><b>Select what most interests you.</b></p>
+                    <ul className="selectOption">
+                        <li className={score4 === 2 ? 'clicked' : ''} onClick={() => handleAnswerSelection(4, 2)}><span>Socialization with fellow modellers</span></li>
+                        <li className={score4 === 4 ? 'clicked' : ''} onClick={() => handleAnswerSelection(4, 4)}><span>Train Operations</span></li>
+                        <li className={score4 === 6 ? 'clicked' : ''} onClick={() => handleAnswerSelection(4, 6)}><span>Building a layout</span></li>
+                        <li className={score4 === 8 ? 'clicked' : ''} onClick={() => handleAnswerSelection(4, 8)}><span>Planning</span></li>
                     </ul>
                     <h3>Results</h3>
-                    <h2>Score: {score[1]}</h2>
-                    <h4>Join a static club</h4>
+                    <h2>Suggestion: </h2>
+                    
+                    {(score3 === 2 || score < 8) && <h4>Join a model railroad club</h4>}
 
-                    <h4>Join a modular club</h4>
+                    {((score3 > 2 && score > 2) || score3 > 4) && <h4>Join a modular club</h4>}
 
-                    <h4>Build a small shelf layout</h4>
+                    {(score3 === 4) &&<h4>Build a small shelf layout</h4>}
 
-                    <h4>Build a layout</h4>
+                    {(score3 > 4) && <h4>Build a layout</h4>}
                 </StyledSection>
             </Block>
         </Layout>
